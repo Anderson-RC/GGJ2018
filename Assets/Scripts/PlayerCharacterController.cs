500bd148ef0aa618131c8c61457d8d0fb6b90faa
hiddenninjaskills <hiddenninjaskills@gmail.com>
2018-01-27 17:21:31 +0000

    vision/player collision layers

---
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,38 @@
+﻿// MoveToClickPoint.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Vector3 destinationEcho;
+    public float stamina = 100.0f;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+
+    void Update()
+    {
+        CheckForMovement();
+    }
+
+
+    void CheckForMovement()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+                destinationEcho = agent.destination;
+            }
+        }
+    }
+}
