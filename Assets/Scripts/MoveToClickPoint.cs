9106c1ff07d239d984e9895f478c6e23dc2916e6
hiddenninjaskills <hiddenninjaskills@gmail.com>
2018-01-27 16:40:32 +0000

    move to click player

---
+++ Assets/Scripts/MoveToClickPoint.cs
@@ -0,0 +1,29 @@
+﻿// MoveToClickPoint.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClickPoint : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Vector3 destinationEcho;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+                destinationEcho = agent.destination;
+            }
+        }
+    }
+}
