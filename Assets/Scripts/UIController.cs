ce460b7ced6c46fb99c26e9bb1d902a96f2fe989
Anderson-RC <ross-anderson@msn.com>
2018-01-28 11:27:03 +0000

    roofs and uis

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIController : MonoBehaviour {
+    public Slider healthBarSlider;
+    public Text message;
+    public Text objectiveCounter;
+
+    private float _stopwatch = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _stopwatch -= Time.deltaTime;
+        if (_stopwatch < 0.0f)
+        {
+            message.enabled = false;
+        } else
+        {
+            message.enabled = true;
+        }
+	}
+    public void SendMessage(string messageText, float timeToShow = 3.0f)
+    {
+        _stopwatch = timeToShow;
+        message.text = messageText;
+    }
+    public void SetObjectiveCounter(int count)
+    {
+        objectiveCounter.text = count.ToString();
+    }
+    public void SetHealthSlider(float healthFraction)
+    {
+        if (healthFraction > 1.0f) { healthBarSlider.value = 1.0f; }
+        if (healthFraction < 0.0f) { healthBarSlider.value = 1.0f; }
+        else { healthBarSlider.value = healthFraction; }
+    }
+}
